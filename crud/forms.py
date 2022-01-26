505f1aefbadf5a864587f0ea269dfd2cf9701326
Alexander Hiebl <webmaster@sarlex-software.com>
2022-01-26 16:36:32 +0100

    clean () for admin form to add admin object if not set

--- crud/forms.py
+++ crud/forms.py
@@ -1,3 +1,4 @@
+from cuser.middleware import CuserMiddleware
 from django import forms
 
 from .models import UserData
@@ -8,4 +9,13 @@ class UserDataForm(forms.ModelForm):
         model = UserData
         fields = '__all__'  # we can set __all__ because cuser is set to editable=False in model
 
+    # When saving data we have to add auth.User to UserData.cuser
+    # clean() has no access to request to get the current user
+    # get user object from CuserMiddleware
+    # test if UserData.cuser is empty (in case if new record or admin who created the userdata has been deleted
+    # (Note: UserData.cuser on_delete is set to SET_NULL to avoid deleting a user only because of deleting the admin)
+    def clean (self):
+
+        if not self.instance.cuser:
+            self.instance.cuser = CuserMiddleware.get_user()
 
